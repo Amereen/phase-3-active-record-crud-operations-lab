c0231e6b6f3b0f37e110a1a5712e1ac989238a14
Amereen Hanvel <amereenhanvel786@gmail.com>
2023-03-30 18:41:15 -0400

    commit tree b3c6d101513b919bd618cb0ff60517c2dd447d1d

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
 class Movie < ActiveRecord::Base
+  def self.create_with_title(title)
+    movie = self.new(title: title)
+    movie.save
+    movie
+  end
+def self.first_movie
+  self.first
+end
+def self.last_movie
+  self.last
+end
+def self.movie_count
+  self.count
+end
+def self.movie_count
+  self.count
+end
+def self.find_movie_with_id(id)
+  self.find(id)
+end
+def self.find_movie_with_attributes(attributes)
+  self.find_by(attributes)
+end
+
+def self.find_movies_after_2002
+  self.where("release_date > ?", 2002)
+end
+ def update_with_attributes(attributes)
+    self.update(attributes)
+  end
+
+  def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+
+def self.delete_by_id(id)
+  movie = Movie.find_by(id: id)
+  movie.destroy 
+end
+def self.delete_all_movies
+ Movie.destroy_all
+end
+
 
 end
